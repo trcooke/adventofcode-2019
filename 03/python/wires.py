9759db864cf86f988d638dfc7085ef4a2af68449
Tim Cooke <tim@trcooke.co.uk>
2019-12-03 11:13:17 +0000

    Day 3 part 2 completed in Python

--- 03/python/wires.py
+++ 03/python/wires.py
@@ -2,59 +2,55 @@ def part1():
     wires = open("input")
     wire1 = wires.readline().replace("\n", "").split(",")
     wire2 = wires.readline().replace("\n", "").split(",")
+    
+    wire1path = getPath(wire1)
+    wire2path = getPath(wire2)
 
-    current = (0,0,0)
-    wire1path = {current}
-    for path in wire1:
-        direction = path[0]
-        distance = int(path[1:])
-        if direction == 'U':
-            for step in range(distance):
-                current = (current[0],current[1] + 1, abs(current[0]) + abs(current[1] + 1))
-                wire1path.add(current)
-        elif direction == 'D':
-            for step in range(distance):
-                current = (current[0],current[1] - 1, abs(current[0]) + abs(current[1] - 1))
-                wire1path.add(current)
-        elif direction == 'L':
-            for step in range(distance):
-                current = (current[0]-1,current[1], abs(current[0] - 1) + abs(current[1]))
-                wire1path.add(current)
-        elif direction == 'R':
-            for step in range(distance):
-                current = (current[0]+1,current[1], abs(current[0] + 1) + abs(current[1]))
-                wire1path.add(current)
-
-    current = (0,0,0)
-    wire2path = {current}
-    for path in wire2:
-        direction = path[0]
-        distance = int(path[1:])
-        if direction == 'U':
-            for step in range(distance):
-                current = (current[0],current[1] + 1, abs(current[0]) + abs(current[1] + 1))
-                wire2path.add(current)
-        elif direction == 'D':
-            for step in range(distance):
-                current = (current[0],current[1] - 1, abs(current[0]) + abs(current[1] - 1))
-                wire2path.add(current)
-        elif direction == 'L':
-            for step in range(distance):
-                current = (current[0]-1,current[1], abs(current[0] - 1) + abs(current[1]))
-                wire2path.add(current)
-        elif direction == 'R':
-            for step in range(distance):
-                current = (current[0]+1,current[1], abs(current[0] + 1) + abs(current[1]))
-                wire2path.add(current)
     closest = 1000000000
-    for intersection in wire1path.intersection(wire2path):
+    for intersection in set(wire1path).intersection(wire2path):
         if intersection[2] < closest and intersection[2] > 0:
             closest = intersection[2]
 
     return closest
 
 def part2():
-    return 0
+    wires = open("input")
+    wire1 = wires.readline().replace("\n", "").split(",")
+    wire2 = wires.readline().replace("\n", "").split(",")
+    
+    wire1path = getPath(wire1)
+    wire2path = getPath(wire2)
+
+    leaststeps = 100000000
+    for intersection in set(wire1path).intersection(wire2path):
+        totalsteps = wire1path.index(intersection) + wire2path.index(intersection)
+        if totalsteps < leaststeps and totalsteps > 0:
+            leaststeps = totalsteps
+    return leaststeps
+
+def getPath(wire):
+    current = (0,0,0)
+    wirepath = [current]
+    for path in wire:
+        direction = path[0]
+        distance = int(path[1:])
+        if direction == 'U':
+            for step in range(distance):
+                current = (current[0],current[1] + 1, abs(current[0]) + abs(current[1] + 1))
+                wirepath.append(current)
+        elif direction == 'D':
+            for step in range(distance):
+                current = (current[0],current[1] - 1, abs(current[0]) + abs(current[1] - 1))
+                wirepath.append(current)
+        elif direction == 'L':
+            for step in range(distance):
+                current = (current[0]-1,current[1], abs(current[0] - 1) + abs(current[1]))
+                wirepath.append(current)
+        elif direction == 'R':
+            for step in range(distance):
+                current = (current[0]+1,current[1], abs(current[0] + 1) + abs(current[1]))
+                wirepath.append(current)
+    return wirepath
 
 print part1()
 print part2()
